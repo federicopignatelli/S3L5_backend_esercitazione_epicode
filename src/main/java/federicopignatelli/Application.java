82598dea235e20c9cf605753df98318087dca176
federicopignatelli <federicopignatelli@icloud.com>
2023-12-15 11:07:30 +0100

    collegamento database

--- src/main/java/federicopignatelli/Application.java
+++ src/main/java/federicopignatelli/Application.java
@@ -1,8 +1,21 @@
 package federicopignatelli;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class Application {
 
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestionebiblioteca");
+
     public static void main(String[] args) {
+        EntityManager em = emf.createEntityManager();
+
+
         System.out.println("Hello World!");
+
+
+        em.close();
+        emf.close();
     }
+
 }
