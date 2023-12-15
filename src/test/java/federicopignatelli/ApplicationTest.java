14102e1e7af2b00e42f50a17a03a0105b9447269
federicopignatelli <federicopignatelli@icloud.com>
2023-12-15 11:00:19 +0100

    progettomaven

---
+++ src/test/java/federicopignatelli/ApplicationTest.java
@@ -0,0 +1,13 @@
+package federicopignatelli;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
