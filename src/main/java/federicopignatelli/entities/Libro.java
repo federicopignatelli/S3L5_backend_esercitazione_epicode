c95aa2ad893677452d593c652805fd22e607ec64
federicopignatelli <federicopignatelli@icloud.com>
2023-12-15 13:35:22 +0100

    creazione entities

--- src/main/java/federicopignatelli/entities/Libro.java
+++ src/main/java/federicopignatelli/entities/Libro.java
@@ -1,4 +1,33 @@
 package federicopignatelli.entities;
 
-public class Libro {
+import java.time.LocalDate;
+
+public class Libro extends ElaboratoTipografico{
+    public String autore;
+    public String genere;
+
+    public Libro() {
+    }
+
+    public Libro(String titolo, String autore, String genere, LocalDate annoPubblicazione, int numeroPagine) {
+        super(titolo, annoPubblicazione, numeroPagine);
+        this.autore = autore;
+        this.genere = genere;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
 }
