100b0291a5e7084e38d2748324a7e7e521db0ace
federicopignatelli <federicopignatelli@icloud.com>
2023-12-15 13:06:51 +0100

    creazione entities

--- src/main/java/federicopignatelli/entities/Rivista.java
+++ src/main/java/federicopignatelli/entities/Rivista.java
@@ -1,5 +1,32 @@
 package federicopignatelli.entities;
 
+import java.time.LocalDate;
+
 public class Rivista extends ElaboratoTipografico{
     public TipoRivista tipoRivista;
+    public String genere;
+
+    public Rivista() {
+    }
+
+    public Rivista(String titolo, String genere, LocalDate annoPubblicazione, int numeroPagine, TipoRivista tipoRivista) {
+        super(titolo, annoPubblicazione, numeroPagine);
+        this.tipoRivista = tipoRivista;
+    }
+
+    public TipoRivista getTipoRivista() {
+        return tipoRivista;
+    }
+
+    public void setTipoRivista(TipoRivista tipoRivista) {
+        this.tipoRivista = tipoRivista;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
 }
